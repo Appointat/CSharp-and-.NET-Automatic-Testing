0b5431fc677c9737c9c5326f20c9b874037c00e7
unknown <appointat@edu.shu.cn>
2022-12-19 01:43:23 +0100

    Create TestNinja.UnitTests

---
+++ TestNinja.UnitTests/Reservation.cs
@@ -0,0 +1,18 @@
+﻿namespace TestNinja.UnitTests
+{   
+    public class Reservation
+    {
+        public User MadeBy { get; set; }
+        public bool CanbeCancelledBy(User user)
+        {
+            return (user.IsAdmin || MadeBy == user);
+            // if (MadeBy == user) return true;
+            // return false;
+        }
+    }
+
+    public class User
+    {
+        public bool IsAdmin { get; set;}
+    }
+}
---
+++ TestNinja.UnitTests/RservationTests.cs
@@ -0,0 +1,48 @@
+﻿// using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Win32;
+using NUnit.Framework;
+using System;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnTrue()
+        {
+            // Arrange 安排
+            var reservation = new Reservation();
+
+            // Act
+            var result = reservation.CanbeCancelledBy(new User { IsAdmin = true });
+
+            // Assert 断言
+            // Assert.IsTrue(result);
+            Assert.That(result, Is.True);
+            // Assert.That(result == true);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_SameUserCancellingTheReservation_RetureTrue()
+        {
+            // Arrange 安排
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+
+            // Act
+            var result = reservation.CanbeCancelledBy(user);
+
+            // Assert
+            Assert.IsTrue(result);            
+        }
+        
+        [Test]
+        public void CanBeCancelledBy_AnotherUserCancellingReservation_ReturnFlase()
+        {
+            var reservation = new Reservation();
+            var result = reservation.CanbeCancelledBy(new User());
+            Assert.IsFalse(result);
+        }
+    }
+}
