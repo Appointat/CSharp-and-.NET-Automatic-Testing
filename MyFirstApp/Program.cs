1fd9e1b420bee78e87c050f17bfeecc3fe32e2c4
unknown <appointat@edu.shu.cn>
2022-12-20 23:39:39 +0100

    Create MyFirstApp

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Dynamic;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var friends = new List<string> { "a", "b", "c" };
+        var partyFriends = 1;
+    }
+}
