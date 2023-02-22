244c335e050901b61b2979d8fb51556711499932
Zikang CHEN <appointat@shu.edu.cn>
2023-02-22 23:49:35 +0100

    Update Data Driven Test

--- DataDrivenTests/DataDrivenTestFixture.cs
+++ DataDrivenTests/DataDrivenTestFixture.cs
@@ -2,20 +2,26 @@
 
 namespace DataDrivenTests
 {
+    // Define a test fixture for data-driven tests
     [TestFixture(10)]
     [TestFixture(42)]
     public class DataDrivenTestFixture
     {
+        // Define a private field to store the test argument
         int _x;
 
+        // Define a constructor to initialize the test argument
         public DataDrivenTestFixture(int x)
         {
             _x = x;
         }
 
+        // Define a test method to use the test argument
         [Test]
         public void TestArguments()
         {
+            // Use the Assert.Pass method to indicate a successful test run
+            // Output the test argument value using string interpolation
             Assert.Pass($"X is {_x}");
         }
     }
--- DataDrivenTests/GenericTestFixture.cs
+++ DataDrivenTests/GenericTestFixture.cs
@@ -2,13 +2,17 @@
 
 namespace DataDrivenTests
 {
+    // Define a generic test fixture for testing different types
     [TestFixture(typeof(int))]
     [TestFixture(typeof(string))]
     public class GenericTestFixture<T>
     {
+        // Define a test method to output the generic test type
         [Test]
         public void TestType()
         {
+            // Use the Assert.Pass method to indicate a successful test run
+            // Output the generic test type using the typeof operator and string interpolation
             Assert.Pass($"The generic test type is {typeof(T)}");
         }
     }
